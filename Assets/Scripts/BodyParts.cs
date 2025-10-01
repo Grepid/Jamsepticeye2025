62d67228bc3ce6c1d8d0f75100a365220d91a78c
Haocheng Mai <lightwizard42@gmail.com>
2025-10-01 12:23:55 -0700

    body parts

---
+++ Assets/Scripts/BodyParts.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BodyParts : MonoBehaviour
+{
+    enum PartType
+    {
+        Legs,
+        Arms,
+        Torso
+    }
+
+    enum Variation
+    {
+        Average,
+        Long,
+        Mutated,
+        Robot,
+        Suit,
+        Construction_Uniform,
+        Bulletproof_Vest,
+        Jacksepticeye_Branded_Tshirt
+    }
+}
