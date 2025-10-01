e9c4c6ea49605e52e86ca1d45175e2f676e92812
Haocheng Mai <lightwizard42@gmail.com>
2025-10-01 11:16:43 -0700

    most of dialogue in
    gonna tween it a bit

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialogueBox;
+    public float typingSpeed = 0.05f;
+    public Button continueButton;
+    public string[] sentences;
+    public string currSentence;
+    bool typing = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sentences = new string[]{
+            "Six years now my mother gone to earth. This dew, light as footsteps of the dead. She often walked out here, craned her neck, considered the fruit, hundreds of globes in their leathery hides, figuring on custard and pudding, meringue and hollandaise.",
+            "But her plans didn't work out.",
+            "The tree goes on unceasingly—lemons fall and fold into earth and begin again— me, I come here as a salve against heat, come to languish, to let the soft bursts—",
+            "essence of citrus, summer's distillate— drift into my face and settle. Water and gold brew in the quiet deeps at the far end of the season. Leaves swallow the body of light and the breath of water brims over.",
+            "My hands cup each other the way hers did."
+        };
+        continueButton.onClick.AddListener(DisplayNextSentence);
+    }
+
+    public void UpdateSentences(string[] newSentences)
+    {
+        sentences = newSentences;
+        EndDialogue();
+    }
+
+    void DisplayNextSentence()
+    {
+        if (typing)
+        {
+            dialogueText.text = currSentence;
+            StopAllCoroutines();
+            typing = false;
+            return;
+        }
+        if (sentences.Length == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences[0];
+        currSentence = sentence;
+        sentences = sentences[1..];
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    void EndDialogue()
+    {
+        dialogueBox.SetActive(false);
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        typing = true;
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        typing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
