aecbb04e381f32a8cc9283f6f170f9f74bb83214
Haocheng Mai <lightwizard42@gmail.com>
2025-10-05 12:27:44 -0700

    credits

--- Assets/Scripts/MollieJones.cs
+++ Assets/Scripts/MollieJones.cs
@@ -9,7 +9,6 @@ public class MollieJones : BaseInteractable
     {
         print($"Interacted with {gameObject.name}");
         // pull up the camera with the 3D model of the body parts
-        Records.playerLastPos = PlayerController.instance.transform.position;
         // SceneChanger.instance.ChangeScene("Operation");
         // lets try this one more time
         SceneChanger.instance.AddScene("Operation");
